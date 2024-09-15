d71a20c5039394966b06a93204ee4777c9d69e5d
olyavitall <kurtikova.ov@gmail.com>
2024-09-15 16:31:13 +0300

    feat: add js file (Sun, Sep 15, 2024 04:31:13 PM)

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = document.querySelector('.slider')
+    const slides = slider.querySelectorAll('.slide')
+    const buttons = slider.querySelectorAll('.btn')
+})
